c508cf1a167e0953052776286aa192583c1db5a7
Young Jeong <yjeong19@gmail.com>
2018-11-14 23:51:30 -0800

    mongo connect

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
-console.log('line 4 models/index.js', keys.mongoURI);
 mongoose.set('debug', true);
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = Promise;
