b681b1e0bbac685d9db8986fb541499e43093685
Young Jeong <yjeong19@gmail.com>
2018-11-14 23:56:51 -0800

    config

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // mongoURI: 'mongodb://yjeong19:soccer1@ds151602.mlab.com:51602/random_restaurant',
+  mongoURI: 'mongodb://heroku_ztg0nwg8:ngfqitncu4mubji2jlgl9kobr3@ds263493.mlab.com:63493/heroku_ztg0nwg8',
+  secretOrKey: 'secret',
+}
