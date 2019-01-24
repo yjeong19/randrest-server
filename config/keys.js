50b3ccf11f835aff58004e91d439c16c5e08c028
Young Jeong <yjeong19@gmail.com>
2019-01-23 16:55:30 -0800

    mongo keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // mongoURI: 'mongodb://yjeong19:soccer1@ds151602.mlab.com:51602/random_restaurant',
+  // mongoURI: 'mongodb://localhost:27017/random_restaurant',
+  //heroku for deploy;
+  mongoURI: 'mongodb://heroku_ztg0nwg8:ngfqitncu4mubji2jlgl9kobr3@ds263493.mlab.com:63493/heroku_ztg0nwg8',
+  secretOrKey: 'secret',
+  MYAPIKEY: 'Bh1GkdpN9hf4X2B784KNXNkwrIIHberMpkM16VUrtqegR4UHhy2M88fHrMwARNF49hd57lz9tDjZV8GKi-iBS9hsJTFU4uSi9S2mivitgNjp6SsxX3zejntqF3RfWnYx',
+}
